d5f26ab27cb8ccc5950fb979455344d4cd90b0ea
Alessio Simoya <asimoya@u.rochester.edu>
2022-06-28 17:41:08 -0400

    Added Basic Routing for the new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div></div>
+import { Route, Switch } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetups'
+import FavoritePage from './pages/Favorites'
+import MainNavigation from './components/layout/MainNavigation'
+const App = () => {
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        {' '}
+        <Route path='/' component={AllMeetupsPage} exact />
+        <Route path='/new-meetup' component={NewMeetupPage} />
+        <Route path='/favorites' component={FavoritePage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const AllMeetups = () => {
+  return <div>AllMeetups</div>
+}
+
+export default AllMeetups
