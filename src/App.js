5193b8ef73339ae6cacedf3c4de7b75caa136643
Alessio Simoya <asimoya@u.rochester.edu>
2022-06-28 18:19:29 -0400

    layouts for the new meetups

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups'
 import NewMeetupPage from './pages/NewMeetups'
 import FavoritePage from './pages/Favorites'
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout'
 const App = () => {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         {' '}
         <Route path='/' component={AllMeetupsPage} exact />
         <Route path='/new-meetup' component={NewMeetupPage} />
         <Route path='/favorites' component={FavoritePage} />
       </Switch>
-    </div>
+    </Layout>
   )
 }
 
